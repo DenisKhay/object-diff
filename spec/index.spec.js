76618c76228b54b8c77a4964d177c2408a925a75
Denis Khaidarshin <denis.khaidarshin@arcadia.spb.ru>
2018-10-18 10:55:41 +0300

    added some cases

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,12 +1,38 @@
 const diff = require('../index');
 
 describe('Object diff testing', () => {
-
-    it('should exist', ()=>{
-        expect(diff).toBeTruthy()
-    })
-
+  it('should exist', () => {
+    expect(diff).toBeTruthy();
+  });
 
 
+  // if params are not valid
+  // what params are not valid ?
 
+  // if params are valid
+  //
+
+  it('should throw an error if input params are not valid or not exists', () => {
+
+  });
+
+  it('should see difference only in base -> target direction', () => {
+
+  });
+
+  it('should see difference between two 1 depth objects', () => {
+
+  });
+
+  it('should see difference between two 1 depth objects', () => {
+
+  });
+
+  it('should not see difference between two 1 depth objects', () => {
+
+  });
+
+  it('should not see difference between 1 depth objects', () => {
+
+  });
 });
